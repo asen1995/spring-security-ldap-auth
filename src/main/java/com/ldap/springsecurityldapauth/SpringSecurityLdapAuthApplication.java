cf312fdf8d24b5f715b4593f8a3b026ef36e0249
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-03-10 15:28:03 +0200

    init commit

---
+++ src/main/java/com/ldap/springsecurityldapauth/SpringSecurityLdapAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ldap.springsecurityldapauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLdapAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityLdapAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ldap/springsecurityldapauth/SpringSecurityLdapAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldap.springsecurityldapauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
